395b9627979c13b2f6ec992f67e5d8d8a6e6dadc
Victor <>
2025-05-23 10:51:30 -0500

    Update Default.vue

--- frontend/src/layouts/Default.vue
+++ frontend/src/layouts/Default.vue
@@ -48,7 +48,7 @@
           <div>
             <h2>Links</h2>
             <ul class="mb-5">
-              <li><a class="no-underline hover:underline" href="https://victorsarchive.com/" target="_blank">Victor's Archive (New version of this website)</a></li>
+              <li><a class="no-underline hover:underline" href="https://victorsarchive.com/" target="_blank">Victor's Archive (New Version of This Website)</a></li>
               <li><a class="no-underline hover:underline" href="https://victorfisher.music/" target="_blank">Victor's Music Page</a></li>
               <li><a class="no-underline hover:underline" href="https://reenchantment.bandcamp.com/" target="_blank">Bandcamp</a></li>
               <li><a class="no-underline hover:underline" href="https://www.youtube.com/@reenchantment/" target="_blank">YouTube</a></li>
